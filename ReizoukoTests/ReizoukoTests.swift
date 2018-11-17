3f61b2ff4e9aa784868f5bbca34f6642b84ce643
工藤彩名 <dempa9_9i6@icloud.com>
2018-11-17 20:31:10 +0900

    Initial Commit

---
+++ ReizoukoTests/ReizoukoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ReizoukoTests.swift
+//  ReizoukoTests
+//
+//  Created by 工藤彩名 on 2018/11/17.
+//  Copyright © 2018 Kudo Ayana. All rights reserved.
+//
+
+import XCTest
+@testable import Reizouko
+
+class ReizoukoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
